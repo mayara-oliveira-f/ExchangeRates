8b6b1b8d794cda9b49b37ea40dcbf95d3194e497
Mayara Oliveira <mayaraferreira.5oliveira@gmail.com>
2026-03-04 12:16:30 -0300

    Initial Commit

---
+++ ExchangeRates/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExchangeRates
+//
+//  Created by Mayara Ferreira de Oliveira on 04/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExchangeRates/ExchangeRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesApp.swift
+//  ExchangeRates
+//
+//  Created by Mayara Ferreira de Oliveira on 04/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
